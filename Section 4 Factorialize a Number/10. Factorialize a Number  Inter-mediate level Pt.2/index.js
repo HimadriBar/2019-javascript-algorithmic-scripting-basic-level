7389ae122c0568540420665297abcb8f46f50239
himadriBar <himadribar4@gmail.com>
2023-05-30 21:21:09 +0530

    10. Factorialize a Number  Inter-mediate level Pt.2
    10. Factorialize a Number  Inter-mediate level Pt.2

---
+++ Section 4 Factorialize a Number/10. Factorialize a Number  Inter-mediate level Pt.2/index.js
@@ -0,0 +1,14 @@
+// Factorialize a number
+// Numbers to be factorialized are 5!, 10!, 20!, 0!
+
+function factorialize(num) {
+    if (num === 0) {
+        return 1
+    }
+    return num * factorialize(num - 1);
+}
+
+console.log(factorialize(5))
+console.log(factorialize(10))
+console.log(factorialize(20))
+console.log(factorialize(0))
